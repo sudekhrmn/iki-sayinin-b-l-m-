974c49461b86151b43e06d2a51f8a716f84100ad
sudekhrmn <nisasudekahramn@gmail.com>
2024-10-20 23:49:25 +0300

    Add files via upload

---
+++ bölüm.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("birinci sayıyı giriniz");
+double sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ikinci sayıyı giriniz");
+double sayi2 = Convert.ToInt32(Console.ReadLine());
+
+double blm = sayi1 / sayi2;
+Console.WriteLine("Birinci sayinin ikinci sayiya bölümü: " + blm);
+Console.ReadKey;
