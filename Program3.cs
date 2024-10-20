7ee0081677b11a4cdff4b353d6151ddd5b0768df
sudekhrmn <nisasudekahramn@gmail.com>
2024-10-20 23:09:12 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("1. sayıyı giriniz");
+int sayi1 = int.Parse(Console.ReadLine());
+Console.WriteLine("2. sayıyı giriniz");
+int sayi2 = int.Parse(Console.ReadLine());
+
+int sonuc = sayi1 / sayi2;
+Console.WriteLine("sayıların bölümü: " +  sonuc);
